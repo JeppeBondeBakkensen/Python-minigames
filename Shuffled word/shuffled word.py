8feee10787c2843e263c10f5a648f82789c7369d
Jeppe Bonde Bakkensen <32620352+JeppeBondeBakkensen@users.noreply.github.com>
2024-10-22 08:03:41 +0200

    Update shuffled word.py

--- Shuffled word/shuffled word.py
+++ Shuffled word/shuffled word.py
@@ -1,50 +1,51 @@
 import random
 
-ord_liste = [
+word_list = [
     "blomster", "fiskeri", "vandring", "klipper", "solskin",
     "håndværk", "fuglsang", "kuglepen", "studenter", "mødelokale",
     "lufthavn", "skrivebord", "arbejdsmiljø", "jernbane", "havemøbler",
     "fællesskab", "billedkunst", "fugleliv", "solnedgang", "cykelsport"
 ]
-# Shufler bogstaverne i et udvalgt ord
-chosen_word = random.choice(ord_liste)
-list_of_char = random.sample(chosen_word, len(chosen_word))
-tekst = "".join(list_of_char)
-print(chosen_word)
 
+# Shuffle the letters in the chosen word
+def shuffle_word(chosen_word):
+    shuffled_letters = random.sample(chosen_word, len(chosen_word))
+    shuffled_text = "".join(shuffled_letters)
+    return shuffled_text
 
-def user_guess():
-    user_input = input(f"Hvilket ord gemmer sig bag {tekst}? ")
+# User guess, what the shuffled word is
+def user_guess(shuffled_word):
+    user_input = input(f"Which word is hidden behind {shuffled_word}? ")
     return user_input
 
+# After each guess this tells the user, what index that are correct
+def correct_indices(user_input, chosen_word):
+    user_input_list = []
+    chosen_word_list = []
+    correct_positions = []
 
-def right_index(input):
-    check_userinput = []
-    chosen_word_as_list = []
-    combined = []
-
-    for char in input:
-        check_userinput.append(char)
+    for char in user_input:
+        user_input_list.append(char)
 
     for char in chosen_word:
-        chosen_word_as_list.append(char)
+        chosen_word_list.append(char)
 
-    for index_1, element_1 in enumerate(check_userinput):
-        for index_2, element_2 in enumerate(chosen_word_as_list):
-            if (element_1 == element_2) and (index_1 == index_2):
-                combined.append(str(index_1 + 1))
+    for user_index, user_char in enumerate(user_input_list):
+        for word_index, word_char in enumerate(chosen_word_list):
+            if (user_char == word_char) and (user_index == word_index):
+                correct_positions.append(str(user_index + 1))
 
-    rigtig_gæt = ", ".join(combined)
-    print(f"Plads {rigtig_gæt} er korrekt")
+    correct_guess = ", ".join(correct_positions)
+    print(f"Position(s) {correct_guess} is/are correct")
 
-
-def hint():
+# After 3 guesses the program ask the user if they want a hint
+def hint(correct_word):
     while True:
         try:
-            want_a_hint = int(input("Press 1 for a hint else press 0: "))
-            if want_a_hint == 1:
-                return print(f"De 3 første bogstaver i ordet er: {chosen_word[:3]}")
-            elif want_a_hint == 0:
+            want_hint = int(input("Press 1 for a hint, else press 0: "))
+            if want_hint == 1:
+                return print(f"The first 3 letters of the word are: {correct_word[:3]}")
+            elif want_hint == 0:
                 user_guess()
             else:
                 print("Enter 1 or 0")
@@ -53,16 +54,20 @@ def hint():
 
 
 def main():
-    number_of_guess = 0
+    guess_count = 0
+    random_word = random.choice(word_list)
+    shuffled = shuffle_word(random_word)
+    print(random_word)  # For debugging
     while True:
-        number_of_guess += 1
-        user_input = user_guess()
-        right_index(user_input)
-        if user_input == chosen_word:
-            print(f"Sådan du fandt ordet {chosen_word} i {number_of_guess}")
+        guess_count += 1
+        user_input = user_guess(shuffled)
+        correct_indices(user_input, random_word)
+        if user_input == random_word:
+            print(f"Well done! You found the word {
+                  random_word} in {guess_count} attempts.")
             break
-        elif number_of_guess >= 3:
-            hint()
+        elif guess_count >= 3:
+            hint(random_word)
         else:
             print("Try again")
 
